55d35dd03992708f503c928caa881838cf7da9aa
inkedawn <inkedawn@gmail.com>
2018-10-10 08:40:07 +0800

    Update utility.go
    fix rand seee problen

--- exec/utility.go
+++ exec/utility.go
@@ -5,16 +5,15 @@ import (
 	"time"
 )
 
-
-var seed = time.Now().UnixNano()
-
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
 
 func randRangeInt63(min, max int64) int64 {
 	return min + rand.Int63()%(max-min+1)
 }
 
 func randSleep(min time.Duration, max time.Duration) {
-	rand.Seed(seed)
 	randNum := randRangeInt63(min.Nanoseconds(), max.Nanoseconds())
 	time.Sleep(time.Duration(randNum) * time.Nanosecond)
 }
