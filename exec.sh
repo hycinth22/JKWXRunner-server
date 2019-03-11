b1c0bdb5aad4c474249cb497933d7a34e176285d
风痕 <inkedawn@gmail.com>
2019-03-11 17:57:33 +0800

    更新便捷脚本

--- exec.sh
+++ exec.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 selfdir=$(dirname $0)
 srcdir=$selfdir
@@ -6,15 +6,6 @@ pidfile=$selfdir/jkwxfucker_exec.pid
 logdir=$selfdir/log
 logfile=$logdir/exec_`date +%Y%m%d_%s_%N`.log
 
-# check running process
-# we haven't implement it yet.
-# 
-# pid=$(cat $pidfile)
-#if [ $pid -ne "" -a $pid -gt 0 ]; then
-#	echo "wait pid $pid exit"
-#	while [ -e /proc/ ]; do sleep 1; done
-# fi'
 
-
-cd $srcdir
-nohup go run $srcdir/exec > $logfile & echo $! > $pidfile
+cd $selfdir
+nohup ./dayexec > $logfile & echo $! > $pidfile
--- restart.sh
+++ restart.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 selfdir=$(dirname $0)
 
