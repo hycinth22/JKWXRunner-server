013292181b557d75dfd833b5841ffb60515b27d7
风痕 <inkedawn@gmail.com>
2019-03-12 18:15:39 +0800

    exec: 处理终止信号

--- exec/main.go
+++ exec/main.go
@@ -5,6 +5,8 @@ import (
 	"github.com/inkedawn/JKWXFucker-server/model"
 	"log"
 	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
@@ -34,7 +36,37 @@ func SleepALitte(totalAccountCount int64, totalExecTime time.Duration) {
 	time.Sleep(d)
 }
 
+func handleExit(c <-chan os.Signal, cExit chan<- bool) {
+	_, ok := <-c
+	if ok {
+		log.Println("Exiting...")
+		log.Println("We need some time to clean.")
+		cExit <- true
+	}
+}
+
+func resumeAccount(acc []*model.Account) {
+	for _, acc := range acc {
+		if acc.LastStatus == model.StatusReadyRun {
+			log.Println("Resume account", acc.Username)
+			acc.LastStatus = model.StatusOK
+			err := model.UpdateAccount(acc)
+			if err != nil {
+				log.Println("resume account error, ", err.Error())
+			}
+		} else {
+			log.Println("Skip account", acc.Username)
+		}
+	}
+}
+
 func RunOnce() {
+	// 监听终止信号
+	exitSigChan := make(chan os.Signal)
+	exitChan := make(chan bool)
+	signal.Notify(exitSigChan, os.Interrupt, os.Kill, syscall.SIGTERM)
+	go handleExit(exitSigChan, exitChan)
+
 	accounts, err := model.ListAccountsTodayNotRun()
 	if len(accounts) == 0 && Debug {
 		log.Println("Debug list all")
@@ -44,17 +76,40 @@ func RunOnce() {
 		log.Println(err.Error())
 		return
 	}
-	for _, account := range accounts {
-		log.Println("Run Task For Account ", account.Username)
-		result := RunForAccount(account)
-		err := saveRunResult(account, result)
-		if err == nil {
-			log.Println("Account ", account.Username, " task run Successfully.")
-		} else {
-			log.Println("Account ", account.Username, " task occur an Error: ", err.Error())
+accountRun:
+	for i, account := range accounts {
+		select {
+		case <-exitChan:
+			resumeAccount(accounts[i:])
+			break accountRun
+		default:
+			log.Println("Run Task For Account ", account.Username)
+			result := RunForAccount(account)
+			err := saveRunResult(account, result)
+			if err == nil {
+				log.Println("Account ", account.Username, " task run Successfully.")
+			} else {
+				log.Println("Account ", account.Username, " task occur an Error: ", err.Error())
+			}
 		}
-		SleepALitte(int64(len(accounts)), 6*time.Hour)
+		sleepOverChan := make(chan bool)
+		go func() {
+			SleepALitte(int64(len(accounts)), 6*time.Hour)
+			sleepOverChan <- true
+		}()
+		select {
+		case <-sleepOverChan:
+		case <-exitChan:
+			log.Println("sleep interrupted")
+			log.Printf("%d account need to resume \n", len(accounts[i:]))
+			resumeAccount(accounts[i:])
+			break accountRun
+		}
 	}
+
+	// 停止监听终止信号，关闭channel通知相关goroutine退出
+	signal.Stop(exitSigChan)
+	close(exitSigChan)
 }
 
 func RunAsDaemon() {
