ab153e08b60a73539e14ac4a93a7166ee22f443b
风痕 <inkedawn@gmail.com>
2019-04-13 12:42:46 +0800

    exec.sh: 现在将即时编译了

--- exec.sh
+++ exec.sh
@@ -2,11 +2,11 @@
 
 params=$*
 selfDir=$(dirname $0)
-srcDir=${selfDir}
+srcDir=${selfDir}/executor
 pidFile=${selfDir}/jkwxfucker_exec.pid
 logDir=${selfDir}/log
 logfile=${logDir}/exec_`date +%Y%m%d_%s_%N`.log
 
 
 cd ${selfDir}
-nohup ./dayexec ${params} > ${logfile} & echo $! > ${pidFile}
+nohup go run ${srcDir} ${params} > ${logfile} & echo $! > ${pidFile}
