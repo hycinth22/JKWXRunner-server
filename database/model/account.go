205f0fcb28b886db14e712fd65dd9e70e561f76c
风痕 <inkedawn@gmail.com>
2019-05-19 09:12:20 +0800

    addMemo

--- database/model/account.go
+++ database/model/account.go
@@ -11,13 +11,14 @@ type Account struct {
 	SchoolID int64  `gorm:"unique_index:schoolID_stuNum;NOT NULL"`
 	StuNum   string `gorm:"unique_index:schoolID_stuNum;NOT NULL"`
 	Password string `gorm:"NOT NULL"`
+	Memo     string `gorm:"NOT NULL;default:''"`
 
+	DeviceID         uint    `gorm:"NOT NULL"`
+	Status           string  `gorm:"NOT NULL;default:'normal''"`
 	RunDistance      float64 `gorm:"NOT NULL"`
 	FinishDistance   float64 `gorm:"NOT NULL;default:0.0"`
 	CheckCheatMarked bool    `gorm:"NOT NULL;default:1"`
 
-	DeviceID   uint      `gorm:"NOT NULL"`
-	Status     string    `gorm:"NOT NULL"`
 	LastResult string    `gorm:"default:''"`
 	LastTime   time.Time `gorm:"default:0"`
 }
