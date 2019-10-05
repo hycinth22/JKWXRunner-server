d79bd4ade7f142c598663ea7ffb2fc02898ef2dc
风痕 <inkedawn@gmail.com>
2019-10-05 11:23:34 +0800

    cors

--- api_server/routers.go
+++ api_server/routers.go
@@ -22,15 +22,12 @@ func init() {
 
 func CORSRouter(router gin.IRouter) {
 	router.Use(func(context *gin.Context) {
-		context.Header("Access-Control-Allow-Origin", "*")
-		context.Header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS")
-
-	})
-	router.OPTIONS("/:all", func(context *gin.Context) {
-		context.Header("Access-Control-Allow-Origin", "*")
-		context.Header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS")
-		context.Header("Access-Control-Allow-Headers", "*")
-		context.Status(http.StatusOK)
+		if context.Request.Method == http.MethodOptions {
+			context.Header("Access-Control-Allow-Origin", "*")
+			context.Header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS")
+			context.Header("Access-Control-Allow-Headers", "*")
+			context.AbortWithStatus(http.StatusOK)
+		}
 	})
 }
 
