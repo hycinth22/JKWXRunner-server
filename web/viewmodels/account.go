d418acee8e1507b29ec45158e82db39241ccee22
风痕 <inkedawn@gmail.com>
2019-11-22 12:02:45 +0800

    EstimateRemainDays

--- web/viewmodels/account.go
+++ web/viewmodels/account.go
@@ -1,43 +1,47 @@
 package viewmodels
 
 import (
+	"math"
+
 	"github.com/inkedawn/JKWXRunner-server/datamodels"
 	"github.com/inkedawn/JKWXRunner-server/viewFormat"
 )
 
 type Account struct {
-	ID                uint
-	OwnerID           int
-	CreatedAt         string
-	SchoolID          int64
-	StuNum            string
-	Memo              string
-	Status            string
-	RunDistance       float64
-	StartDistance     float64
-	FinishDistance    float64
-	CurrentDistance   float64
-	QualifiedDistance float64
-	CheckCheatMarked  bool
-	LastResult        string
-	LastTime          string
+	ID                 uint
+	OwnerID            int
+	CreatedAt          string
+	SchoolID           int64
+	StuNum             string
+	Memo               string
+	Status             string
+	RunDistance        float64
+	StartDistance      float64
+	FinishDistance     float64
+	CurrentDistance    float64
+	QualifiedDistance  float64
+	EstimateRemainDays int `json:"estimate_remain_days"`
+	CheckCheatMarked   bool
+	LastResult         string
+	LastTime           string
 }
 
 func AccountView(acc *datamodels.Account, currentDistance, qualifiedDistance float64) *Account {
 	view := &Account{
-		ID:                acc.ID,
-		OwnerID:           acc.OwnerID,
-		CreatedAt:         viewFormat.TimeFormat(acc.CreatedAt),
-		SchoolID:          acc.SchoolID,
-		StuNum:            acc.StuNum,
-		Memo:              acc.Memo,
-		Status:            acc.Status,
-		RunDistance:       acc.RunDistance,
-		StartDistance:     acc.StartDistance,
-		FinishDistance:    acc.FinishDistance,
-		CurrentDistance:   currentDistance,
-		QualifiedDistance: qualifiedDistance,
-		CheckCheatMarked:  acc.CheckCheatMarked.Valid && acc.CheckCheatMarked.Bool,
+		ID:                 acc.ID,
+		OwnerID:            acc.OwnerID,
+		CreatedAt:          viewFormat.TimeFormat(acc.CreatedAt),
+		SchoolID:           acc.SchoolID,
+		StuNum:             acc.StuNum,
+		Memo:               acc.Memo,
+		Status:             acc.Status,
+		RunDistance:        acc.RunDistance,
+		StartDistance:      acc.StartDistance,
+		FinishDistance:     acc.FinishDistance,
+		CurrentDistance:    currentDistance,
+		QualifiedDistance:  qualifiedDistance,
+		EstimateRemainDays: int(math.Ceil((acc.FinishDistance - currentDistance) / acc.RunDistance)),
+		CheckCheatMarked:   acc.CheckCheatMarked.Valid && acc.CheckCheatMarked.Bool,
 	}
 	if acc.LastResult.Valid {
 		view.LastResult = acc.LastResult.String
